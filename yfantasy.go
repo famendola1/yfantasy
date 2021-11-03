20c3d5b01150312951f8b28cc3f84889d502e895
Fabio Amendola <fabio.amendola20@gmail.com>
2021-11-02 22:44:09 -0400

    GetLeagueRaw

--- yfantasy.go
+++ yfantasy.go
@@ -16,7 +16,14 @@ type YFantasy struct {
 	client *http.Client
 }
 
-// GetGameRaw queries the Yahoo Fantasy API /game endpoint and returns the raw
+// processAPIResponse converts the response body into a string.
+func processAPIResponse(resp *http.Response) string {
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(resp.Body)
+	return buf.String()
+}
+
+// GetGameRaw queries the /game endpoint for game data and returns the raw
 // response as an XML string.
 // Valid inputs for sport are: nba, nfl, mlb, nhl
 func (y *YFantasy) GetGameRaw(sport string) (string, error) {
@@ -24,7 +31,16 @@ func (y *YFantasy) GetGameRaw(sport string) (string, error) {
 	if err != nil {
 		return "", err
 	}
-	buf := new(bytes.Buffer)
-	buf.ReadFrom(resp.Body)
-	return buf.String(), nil
+	return processAPIResponse(resp), nil
+}
+
+// GetLeagueRaw queries the /league endpoint for league information and returns
+// the raw response as an XML string.
+// leagueKey has the format: <game_key>.l.<league_id> (ex: nba.l.1234)
+func (y *YFantasy) GetLeagueRaw(leagueKey string) (string, error) {
+	resp, err := y.client.Get(fmt.Sprintf("%v/league/%v", endpoint, leagueKey))
+	if err != nil {
+		return "", err
+	}
+	return processAPIResponse(resp), nil
 }
